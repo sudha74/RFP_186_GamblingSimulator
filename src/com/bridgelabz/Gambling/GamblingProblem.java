9fbbc960a569868c3f9c86996ed8f235ab6d30d0
sudha123 <sudhanaukala1997@gmail.com>
2022-08-19 07:29:58 +0530

    Start_Stake

---
+++ src/com/bridgelabz/Gambling/GamblingProblem.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.Gambling;
+
+public class GamblingProblem {
+    static int DAILY_STAKE = 100;
+    static int PER_DAY_BET = 1;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Gambling Simulator");
+
+    }
+}
